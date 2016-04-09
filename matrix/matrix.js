5a70f9888a2c0b8f7feba1bae7ac1ccd2f2c5d27
Xiaoyun <yangx232@gmail.com>
2016-04-09 16:49:01 -0400

    added tooltip and icon representations for mediatypes

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,10 +1,13 @@
-var margin = { top: 50, right: 0, bottom: 100, left: 50 },
+var margin = { top: 80, right: 0, bottom: 100, left: 80 },
     padding = 2;
     width = 960 - margin.left - margin.right,
     height = 430 - margin.top - margin.bottom,
     gridSize = Math.floor(width / 24),
     legendElementWidth = gridSize*2,
+    tooltipHeight = 20,
     buckets = 9,
+    mediaIcons = {Image: '\ue3f4', Video: '\ue04a', Answer: '\ue8fd', Article: '\ue02f', Code: '\ue86f', Tutorial: '\ue8fd'  } //Materials
+    //mediaIcons = {Image: '\uf03e', Video: '\uf16a', Answer: '\uf059', Article: '\ue02f', Code: '\uf121', Tutorial: '\ue8fd'  } //FontAwesome
     colors = ["#f7fcf5","#e5f5e0","#c7e9c0","#a1d99b","#74c476","#41ab5d","#238b45","#006d2c","#00441b"], // alternatively colorbrewer.YlGnBu[9]
     datasets = ["data.csv", "data2.csv"];
 
@@ -21,6 +24,7 @@ var svg = d3.select("#chart").append("svg")
 var heatmapChart = function(data) {
     var tags = _.unique(data.map(function(d){return objVal(d,0)}));
     var media = _.unique(data.map(function(d){return objVal(d,1)}));
+    var values = data.map(function(d){return objVal(d,2)});
     var labels = {};
     labels.tags = tags;
     labels.media = media;
@@ -41,22 +45,27 @@ var heatmapChart = function(data) {
     var mediaLabels = svg.selectAll(".media-label")
         .data(labels.media)
         .enter().append("text")
-        .text(function(d) { return d; })
+        .attr("font-family","FontAwesome")
+        .text(function(d) { return mediaIcons[d]; })
+        .attr("value", function(d) {return d;})
+        //.text(function(d) { return d; })
         .attr("x", function(d, i) { return i * gridSize; })
         .attr("y", 0)
-        .attr("value", function(d) {return d;})
-        .style("text-anchor", "middle")
-        .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-        .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "media-label mono axis axis-mediatype" : "media-label mono axis"); });
+        .attr("class", "media-label axis-mediatype")
+        .attr("transform", "translate(" + gridSize / 6 + ", -2)")
+        //.style("text-anchor", "middle");
+
+        //.attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "media-label mono axis axis-mediatype" : "media-label mono axis"); })
+
 
     var colorScale = d3.scale.quantile()
-        .domain([0, buckets - 1, d3.max(data, function (d) { return objVal(d,2); })])
+        .domain([0, d3.max(data, function (d) { return parseFloat(objVal(d,2)); })])
         .range(colors);
 
     var boxes = svg.selectAll(".box")
         .data(data, function(d) {return objVal(d,0)+':'+objVal(d,1);});
 
-    boxes.append("title");
+    //boxes.append("title"); //The tooltip
 
     boxes.enter().append("rect")
         .attr("x", function(d) { return (_.indexOf(labels.media, objVal(d,1))) * gridSize + padding; })
@@ -75,30 +84,58 @@ var heatmapChart = function(data) {
 
     boxes.exit().remove();
 
+
     //Interaction with the boxes
     svg.selectAll(".box").on("mouseover", boxMouseover);
 
+
     function boxMouseover(d,i) {
       resetGrid();
-
       //highlight selected box and corresponding labels
       d3.select(this).classed("selected-bordered", true);
-      mediaLabels.filter(function(m) {return m == objVal(d,1)}).classed("selected-mediatype", true);
+      mediaLabels.filter(function(m) {return m == objVal(d,1)})
+                 .classed("selected-mediatype", true)
+                 .attr("transform", "translate(" + gridSize / 8 + ", -2)");
       tagLabels.filter(function(m) {return m == objVal(d,0)}).classed("selected-taglabel", true);
     }
 
     function resetGrid() {
       //reset everything
       svg.selectAll(".box").classed("selected-bordered", false);
-      svg.selectAll(".media-label").classed("selected-mediatype", false);
+      svg.selectAll(".media-label")
+         .classed("selected-mediatype", false)
+         .attr("transform", "translate(" + gridSize / 6 + ", -2)");
       svg.selectAll(".tag-label").classed("selected-taglabel", false);
     }
+
     //Interaction with the media label
-    svg.selectAll(".media-label").on("mouseover", mediaLabelMouseover);
+    svg.selectAll(".media-label")
+       .on("mouseover", mediaLabelMouseover)
+       .on("mouseout", mediaLabelMouseout);;
     function mediaLabelMouseover(d) {
       resetGrid();
-      d3.select(this).classed("selected-mediatype", true);
+      d3.select(this)
+        .classed("selected-mediatype", true)
+        .attr("transform", "translate(" + gridSize / 8 + ", -2)");
       //console.log(this.getAttribute("value"));
+      //add tooltip//Get this bar's x/y values, then augment for the tooltip
+      var xPosition = parseFloat(d3.select(this).attr("x"))+margin.left-gridSize/2;
+      //var yPosition = d3.event.pageY-tooltipHeight;
+      var yPosition = $("#chart").position().top + parseFloat(d3.select(this).attr("y"))-tooltipHeight;
+      //Update the tooltip position and value
+      d3.select("#tooltip")
+        .style("left", xPosition  + "px")
+        .style("top", yPosition + "px")
+        .select("#value")
+        .text(d);
+
+      //Show the tooltip
+      d3.select("#tooltip").classed("hidden", false);
+
+    }
+    function mediaLabelMouseout(d,i) {
+      //Hide the tooltip
+      d3.select("#tooltip").classed("hidden", true);
     }
     //Interaction with the tag label
     svg.selectAll(".tag-label").on("mouseover", tagLabelMouseover);
@@ -114,17 +151,17 @@ var heatmapChart = function(data) {
         .attr("class", "legend");
 
     legend.append("rect")
-      .attr("x", function(d, i) { return legendElementWidth * i; })
-      .attr("y", height)
-      .attr("width", legendElementWidth)
-      .attr("height", gridSize / 2)
-      .style("fill", function(d, i) { return colors[i]; });
+          .attr("x", function(d, i) { return legendElementWidth * i; })
+          .attr("y", height)
+          .attr("width", legendElementWidth)
+          .attr("height", gridSize / 2)
+          .style("fill", function(d, i) { return colors[i]; });
 
     legend.append("text")
-      .attr("class", "mono")
-      .text(function(d) { return "≥ " + Math.round(d); })
-      .attr("x", function(d, i) { return legendElementWidth * i; })
-      .attr("y", height + gridSize);
+          .attr("class", "mono")
+          .text(function(d) { return "≥ " + Math.round(d); })
+          .attr("x", function(d, i) { return legendElementWidth * i; })
+          .attr("y", height + gridSize);
 
     legend.exit().remove();
 };
