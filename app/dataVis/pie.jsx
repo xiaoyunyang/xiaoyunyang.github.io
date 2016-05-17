f5a109d33cb772427066fb126411cb3a2bd00023
Xiaoyun <yangx232@gmail.com>
2016-05-17 01:21:29 -0400

    added a link to pie.html from main app

--- app/dataVis/pie.jsx
+++ app/dataVis/pie.jsx
@@ -13,6 +13,8 @@ var Pie = React.createClass({
     return (
     <div>
       <div className="col s12 l10">
+        <h4>Piechart DataVis:</h4>
+        <p>Click <a href="piechart/pie-collection.html">HERE</a> for template</p>
         <h5>2016 Officer Monthly Income Breakdown</h5>
         <p>The BAH rate is based on DC metro O2 without dependents.
           Source: <a target="_blank" href="http://www.stripes.com/polopoly_fs/1.384271.1450203758!/menu/standard/file/2016%20%20BAH%20Rates.pdf">2016 BAH Rate </a>
