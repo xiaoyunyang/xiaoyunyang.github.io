ca5f2c27995cb5d18c63ca16ee39294547596361
Xiaoyun <yangx232@gmail.com>
2016-06-17 23:03:06 -0400

    added profile photos for collection owners

--- app/main/bookmarks.jsx
+++ app/main/bookmarks.jsx
@@ -26,20 +26,28 @@ var BookmarksList = React.createClass({
         <div className="col s12 m8 l4">
           <h5>Bookmark Collections</h5>
           <ul className="collection">
-            <li className="collection-item">
-              <a href="index.html">LooseLeaf</a>
+            <li className="collection-item avatar">
+              <img className="square" src="../assets/data/profile/photo/looseleaf.png" />
+              <span className="title"><a href="index.html">LooseLeaf</a></span>
+              <p>A new delivery vehicle for educational content online</p>
               <a target="_blank" href="https://docs.google.com/spreadsheets/d/1X9PrlVeO9GCSmHw8psSD1fN_77vueV_A4EdaQrVzfO0/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
             </li>
-            <li className="collection-item">
-              <a href="user-xyang.html">Xiaoyun Yang</a>
+            <li className="collection-item avatar">
+              <img className="square" src="../assets/data/profile/photo/xyang.png" />
+              <span className="title"><a href="user-xyang.html">Xiaoyun Yang</a></span>
+              <p>Entrepreneur, engineer, founder of LooseLeaf</p>
               <a target="_blank" href="https://docs.google.com/spreadsheets/d/1X9PrlVeO9GCSmHw8psSD1fN_77vueV_A4EdaQrVzfO0/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
             </li>
-            <li className="collection-item">
-              <a href="user-afenner.html">Andrew Fenner</a>
+            <li className="collection-item avatar">
+              <img className="square" src="../assets/data/profile/photo/afenner.png" />
+              <span className="title"><a href="user-afenner.html">Andrew Fenner</a></span>
+              <p>Entrepreneur, math teacher, founder of NumberShapes</p>
               <a target="_blank" href="https://docs.google.com/spreadsheets/d/1_vSN2wfjtuTwbn6UJVJpoO3Epw71c-pigSpIpNipN6g/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
             </li>
-            <li className="collection-item">
-              <a href="user-numbershapes.html">NumberShapes</a>
+            <li className="collection-item avatar">
+              <img className="square" src="../assets/data/profile/photo/numbershapes.png" />
+              <span className="title"><a href="user-numbershapes.html">NumberShapes</a></span>
+              <p>The best digital manipulative apps with which to teach early math on the app store.</p>
               <a target="_blank" href="https://docs.google.com/spreadsheets/d/1_fJp1is2UAV5hut4WfOqhWQMWAT2TyKooB8HUpLl1cA/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
             </li>
           </ul>
