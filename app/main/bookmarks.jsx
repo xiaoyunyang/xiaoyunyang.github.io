bc05eb5e5d45064c0d98a94652a06c694ae414a1
Xiaoyun <yangx232@gmail.com>
2016-08-21 16:35:04 -0400

    spreadsheet csv updated

--- app/main/bookmarks.jsx
+++ app/main/bookmarks.jsx
@@ -26,25 +26,25 @@ var BookmarksList = React.createClass({
               <img className="square" src="../assets/data/profile/photo/looseleaf.png" />
               <span className="title"><a href="dashboard.html">LooseLeaf</a></span>
               <p>A new delivery vehicle for educational content online</p>
-              <a target="_blank" href="https://docs.google.com/spreadsheets/d/1qSz2Pn7WWVqSuUfxy1u8nWT9duv4WJ_24p1Xr2D_cTY/edit#gid=0" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
+              <a target="_blank" href="https://docs.google.com/spreadsheets/d/1dY9xXzn4AtzvlhlhMO24xhe53KWr5pnNKVckM3YboKE/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
             </li>
             <li className="collection-item avatar">
               <img className="square" src="../assets/data/profile/photo/xyang.png" />
               <span className="title"><a href="user-xyang.html">Xiaoyun Yang</a></span>
               <p>Entrepreneur, engineer, founder of LooseLeaf</p>
-              <a target="_blank" href="https://docs.google.com/spreadsheets/d/1X9PrlVeO9GCSmHw8psSD1fN_77vueV_A4EdaQrVzfO0/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
+              <a target="_blank" href="https://docs.google.com/spreadsheets/d/1K7Wxt2Ma6ksU8LOSvksJioPcApZISssxU5ceOx7CENQ/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
             </li>
             <li className="collection-item avatar">
               <img className="square" src="../assets/data/profile/photo/afenner.png" />
               <span className="title"><a href="user-afenner.html">Andrew Fenner</a></span>
               <p>Entrepreneur, math teacher, founder of NumberShapes</p>
-              <a target="_blank" href="https://docs.google.com/spreadsheets/d/1_vSN2wfjtuTwbn6UJVJpoO3Epw71c-pigSpIpNipN6g/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
+              <a target="_blank" href="https://docs.google.com/spreadsheets/d/1-GDFZYik7_FQIJ4GTTDcOYbl8590BNs-pvDOcApQSI4/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
             </li>
             <li className="collection-item avatar">
               <img className="square" src="../assets/data/profile/photo/numbershapes.png" />
               <span className="title"><a href="user-numbershapes.html">NumberShapes</a></span>
               <p>The best digital manipulative apps with which to teach early math on the app store.</p>
-              <a target="_blank" href="https://docs.google.com/spreadsheets/d/1_fJp1is2UAV5hut4WfOqhWQMWAT2TyKooB8HUpLl1cA/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
+              <a target="_blank" href="https://docs.google.com/spreadsheets/d/1B-8MiNx8WyPAHEIZT3hOgmpcY_B6vECrOxHaQY9nFRw/edit?usp=sharing" className="secondary-content"><i className="material-icons">&#xE254;</i></a>
             </li>
           </ul>
         </div>
