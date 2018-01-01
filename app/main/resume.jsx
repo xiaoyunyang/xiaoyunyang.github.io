765c43c718f7900e9b66461cf556af62a2cca24d
Xiaoyun <yangx232@gmail.com>
2018-01-01 16:47:23 -0500

    added link to resume

--- app/main/resume.jsx
+++ app/main/resume.jsx
@@ -13,6 +13,11 @@ var Resume = React.createClass({
     return (
       <div>
         <div className="col s12 m12 l12">
+          <p><i className="fa fa-link"></i>
+            <a href="https://drive.google.com/file/d/0B_uByl2mOTJIVTM3ZWRYRVpIcWs/view?usp=sharing">  Most Current Resume</a>
+          </p>
+        </div>
+        <div className="col s12 m12 l12">
           <iframe src="/assets/data/profile/resume.pdf"></iframe>
         </div>
       </div>
