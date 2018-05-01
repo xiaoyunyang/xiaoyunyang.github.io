c7e22bb037890c756c4fda69110227b073867450
Xiaoyun <yangx232@gmail.com>
2018-04-30 21:32:36 -0400

    rebuilding site Mon Apr 30 21:32:36 EDT 2018

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,18 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+# Commit to the sourcecode
+git checkout sourcecode
+git add .
+git commit -m "$msg"
+git push origin sourcecode
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
@@ -12,17 +24,12 @@ cd public
 # then it will get displayed instead of your actual website
 rm README.md
 
+git checkout master
 # Add changes to git.
 git add .
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-
 # Make sure we're on the master branch before committing compiled site
-git checkout master
+
 git commit -m "$msg"
 
 # Push source and build repos.
@@ -30,9 +37,3 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
-
-# Commit to the sourcecode
-git checkout sourcecode
-git add .
-git commit -m "$msg"
-git push origin sourcecode
