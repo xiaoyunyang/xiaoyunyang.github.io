afb4f914e65a7c3a6a25435e975e650ad8593f11
Xiaoyun <yangx232@gmail.com>
2018-04-30 21:38:55 -0400

    rebuilding site Mon Apr 30 21:38:55 EDT 2018

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,28 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
-
-# If REAME.md is at the base of the folder,
-# then it will get displayed instead of your actual website 
-rm README.md
-
-# Add changes to git.
-git add .
-
-# Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
+# Commit to the sourcecode branch
+rm -f .git/index.lock
+git checkout sourcecode
+git add .
+git commit -m "$msg"
+git push origin sourcecode
+
+# Commit to the master branch
+cd public
+
+# If REAME.md is at the base of the folder,
+# then it will get displayed instead of your actual website
+rm README.md
+
+git checkout master
+# Add changes to git.
+git add .
 git commit -m "$msg"
 
 # Push source and build repos.
